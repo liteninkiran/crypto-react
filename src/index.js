e1d0c9d2e0f249502d52734fa86654d205c6d3b8
Kiran Anand <liteninkiran@hotmail.com>
2021-09-25 23:45:34 +0100

    Adding store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import store from './app/store';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
