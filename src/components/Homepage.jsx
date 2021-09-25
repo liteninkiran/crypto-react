c2c34ef1784c778cdf8ff36c57f204b942fb214b
Kiran Anand <liteninkiran@hotmail.com>
2021-09-26 00:13:52 +0100

    Create homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+// import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+import Loader from './Loader';
+
+const { Title } = Typography;
 
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery(10);
+    // const globalStats = data?.data?.stats;
+
+    console.log(data);
+
+    if (isFetching) return <Loader />;
+
     return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
+        <>
+            {/* Global Stats */}
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+
+            <Row gutter={[32, 32]}>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" /></Col>
+                <Col span={12}><Statistic title="Total Market Cap:" /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" /></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
+                <Col span={12}><Statistic title="Total Markets" /></Col>
+            </Row>
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+
+            {/* <Cryptocurrencies simplified /> */}
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3}><Link to="/news">Show more</Link></Title>
+            </div>
+
+            {/* News */}
+            <News simplified />
+        </>
     );
-}
+};
 
 export default Homepage;
