c44bb2c4eb49f06c2613f3c88a91f29f7b066a55
Kiran Anand <liteninkiran@hotmail.com>
2021-09-25 23:13:34 +0100

    Create navbar and basic component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, CryptoCurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className="app">
+
+            {/* Navbar */}
+            <div className="navbar">
+                <Navbar />
+            </div>
+
+            {/* Main Content */}
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/" component={Homepage} />
+                            <Route exact path="/exchanges" component={Exchanges} />
+                            <Route exact path="/cryptocurrencies" component={CryptoCurrencies} />
+                            <Route exact path="/crypto/:coinId" component={CryptoDetails} />
+                            <Route exact path="/news" component={News} />
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
+
+            {/* Footer */}
+            <div className="footer">
+            
+            </div>
+
         </div>
     );
 }
