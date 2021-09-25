b2f86e2b9696c3906c8ed8ad9df8dc9b2527708e
Kiran Anand <liteninkiran@hotmail.com>
2021-09-26 00:22:13 +0100

    Finish homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
-// import Cryptocurrencies from './Cryptocurrencies';
+import CryptoCurrencies from './CryptoCurrencies';
 import News from './News';
 import Loader from './Loader';
 
@@ -12,9 +12,7 @@ const { Title } = Typography;
 
 const Homepage = () => {
     const { data, isFetching } = useGetCryptosQuery(10);
-    // const globalStats = data?.data?.stats;
-
-    console.log(data);
+    const globalStats = data?.data?.stats;
 
     if (isFetching) return <Loader />;
 
@@ -24,21 +22,23 @@ const Homepage = () => {
             <Title level={2} className="heading">Global Crypto Stats</Title>
 
             <Row gutter={[32, 32]}>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" /></Col>
-                <Col span={12}><Statistic title="Total Market Cap:" /></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" /></Col>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
-                <Col span={12}><Statistic title="Total Markets" /></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalStats.totalMarketCap)} /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
 
+            {/* Top 10 Cryptos */}
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
             </div>
 
-            {/* <Cryptocurrencies simplified /> */}
+            {/* Crypto Currencies */}
+            <CryptoCurrencies simplified />
 
+            {/* News */}
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
                 <Title level={3}><Link to="/news">Show more</Link></Title>
